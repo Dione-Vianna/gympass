d56ef5676cc432b1120dc0e63fb9dd3f2a3fcf32
dione-vianna <dioneg12@hotmail.com>
2023-03-10 15:36:50 -0300

    :test_tube: Add a failing test.
    teste repo create user

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,18 +1,51 @@
-import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
-import { describe, it } from 'vitest'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { Prisma } from '@prisma/client'
+import { compare } from 'bcryptjs'
+import { describe, expect, it } from 'vitest'
 import { RegisterUseCase } from './register'
+import { UserAlreadyExistsError } from './user-already-exists-error'
 
 describe('Register Use Case', async () => {
-  it('should hash user password upon registration', async () => {
-    const prismaUsersRepository = new PrismaUsersRepository()
-    const registerUseCase = new RegisterUseCase(prismaUsersRepository)
+  let state: any = {
+    name: 'John Doe',
+    password: '123456',
+    email: 'johndoe3@example.com',
+  }
 
+  const usersRepository = new InMemoryUsersRepository()
+  const registerUseCase = new RegisterUseCase(usersRepository)
+
+  it('should be able to register', async () => {
     const { user } = await registerUseCase.execute({
-      name: 'John Doe',
-      email: 'johndoe@example.com',
-      password: '123456',
+      name: state.name,
+      email: state.email,
+      password: state.password,
     })
 
-    console.log(user.password_hash)
+    state = {
+      ...state,
+      ...user,
+    }
+
+    expect(user.id).toEqual(expect.any(String))
+  })
+
+  it('should hash user password upon registration', async () => {
+    const isPasswordCorrectlyHashed = await compare(
+      state.password,
+      state.password_hash
+    )
+
+    expect(isPasswordCorrectlyHashed).toBe(true)
+  })
+
+  it('should not be able to register with same email twice', async () => {
+    await expect(() =>
+      registerUseCase.execute({
+        name: state.name,
+        email: state.email,
+        password: state.password,
+      })
+    ).rejects.toBeInstanceOf(UserAlreadyExistsError)
   })
 })
